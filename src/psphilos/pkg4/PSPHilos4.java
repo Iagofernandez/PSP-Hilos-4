06c0a9deef8f306835b71ecdf6ea053132d6fd43
dam2 <ifernandezblanco@danielcastelao.org>
2019-10-25 10:42:56 +0200

    Ejercicio PSP 4: hilos Falta por acabar??

---
+++ src/psphilos/pkg4/PSPHilos4.java
@@ -0,0 +1,49 @@
+
+package psphilos.pkg4;
+
+public class PSPHilos4 extends Thread{
+public PSPHilos4(String str){
+   
+    super(str);
+    }
+//hilo que realiza la suma de los numeros pares del 1 al 1000
+   //hilo que relaiza la suma de los numeros impares del 1 al 1000(mas  o menos)
+    //falta hilo que sume numeros acabados en 2 o 3 (falta)
+    
+    @Override
+    public void run() {
+        super.run();
+        
+        int sumaPar=0;
+        String cadeaPar = "";
+        
+        for(int i = 0;i<1000;i=i+2){
+            cadeaPar=cadeaPar+ " "+i;
+            sumaPar = sumaPar +i;
+        }
+        System.out.println(cadeaPar);
+        System.out.println(sumaPar);
+        
+    int sumaImp = 0;
+    String cadeaImp ="";
+    for(int y=0;y<1000;y=y+1){
+        if(y%2!=0){
+            cadeaImp =cadeaImp+" "+y;
+            sumaImp = sumaImp+y;
+        
+        }
+        
+        System.out.println(cadeaImp);
+        System.out.println(sumaImp);
+    }
+    
+    }
+    public static void main(String[] args) {
+       PSPHilos4 h1 = new PSPHilos4("Suma pares");
+   
+   PSPHilos4 h2 = new PSPHilos4("suma impares");
+   h1.start();
+   h2.start();
+    }
+    
+}
